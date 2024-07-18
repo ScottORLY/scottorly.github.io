34c14833e33711d4c5b3c53208774c05bd9104a4
scottorly <scott.orlyck@gmail.com>
2024-07-18 06:59:29 -0700

    times projection

--- src/map/index.jsx
+++ src/map/index.jsx
@@ -1,12 +1,13 @@
 
 import { geoPath, geoMercator, geoGraticule } from 'd3-geo'
+import { geoTimes } from 'd3-geo-projection'
 import { selectAll  } from 'd3-selection'
 import { feature } from 'topojson-client'
 import land from './land-10m.json'
 
 const createMap = () => {
     const lands = feature(land, land.objects.land)
-    const path = geoPath(geoMercator().fitExtent([[0, 40], [620, 420]], lands).scale(100))
+    const path = geoPath(geoTimes().fitExtent([[-40, -40], [620, 420]], lands).scale(150))
     const graticuleGenerator = geoGraticule().step([10,10])
     const g = selectAll('#map')
     const graticules = selectAll('#graticules')
